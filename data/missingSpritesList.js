796bc7889afd7a58516a5ab9622292a4ceacb3b0
joseph-moniz <58448341+joseph-moniz@users.noreply.github.com>
2024-05-01 21:12:14 -0400

    Created list for gen 9 Pokemon without Smogon sprites

---
+++ data/missingSpritesList.js
@@ -0,0 +1,12 @@
+exports.list = [
+    "Wooper-Paldea", "Tauros-Paldea-Combat", "Tauros-Paldea-Aqua", "Tauros-Paldea-Blaze", "Pawmi", "Pawmo", "Pawmot", "Tandemaus", "Maushold", 
+    "Fidough", "Dachsbun", "Squawkabilly", "Nacl", "Naclstack", "Garganacl", "Charcadet", "Armarouge", "Ceruledge", "Tadbulb", "Bellibolt", 
+    "Wattrel", "Kilowattrel", "Maschiff", "Mabosstiff", "Shroodle", "Grafaiai", "Bramblin", "Brambleghast", "Toedscool", "Toedscruel", "Klawf", 
+    "Tinkatink", "Tinkatuff", "Tinkaton", "Bombirdier", "Varoom", "Revavroom", "Cyclizar", "Orthworm", "Glimmet", "Glimmora", "Flamigo", 
+    "Cetoddle", "Cetitan", "Tatsugiri", "Annihilape", "Clodsire", "Kingambit", "Great Tusk", "Scream Tail", "Brute Bonnet", "Flutter Mane", 
+    "Slither Wing", "Sandy Shocks", "Iron Treads", "Iron Bundle", "Iron Hands", "Iron Jugulis", "Iron Moth", "Iron Thorns", "Frigibax", "Arctibax",
+    "Baxcalibur", "Gimmighoul", "Gimmighoul-Roaming", "Gholdengo", "Wo-Chien", "Chien-Pao", "Ting-Lu", "Chi-Yu", "Roaring Moon", "Iron Valiant", 
+    "Koraidon", "Miraidon", "Walking Wake", "Iron Leaves", "Dipplin", "Poltchageist", "Sinistcha", "Okidogi", "Munkidori", "Fezandipiti", 
+    "Ogerpon", "Ogerpon-Wellspring", "Ogerpon-Hearthflame", "Ogerpon-Cornerstone", "Archaludon", "Hydrapple", "Gouging Fire", "Raging Bolt", 
+    "Iron Boulder", "Iron Crown", "Terapagos", "Terapagos-Terastal", "Terapagos-Stellar", "Pecharunt",
+]
